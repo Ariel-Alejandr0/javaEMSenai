9041da7ade9dfbb890bf15eebe4f7a492474528a
ARIEL ALEJANDRO MARCELLINO SILVA <ariel_a_silva@estudante.sesisenai.org.br>
2024-03-27 08:28:52 -0300

    Atualizando a prova

--- ProjetoAviaoAriel/src/projetoaviaoariel/ProjetoAviaoAriel.java
+++ ProjetoAviaoAriel/src/projetoaviaoariel/ProjetoAviaoAriel.java
@@ -1,56 +1,109 @@
+
 package projetoaviaoariel;
 
 import java.util.Scanner;
 
-
+/**
+ *
+ * @author ariel_a_silva
+ */
 public class ProjetoAviaoAriel {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // invocando classe scanner
         
-        Scanner sc = new Scanner(System.in);
+        //inicializando variáveis
+        String[] vetLugares          = new String[20];
+        boolean[] vetLugaresOcupados = new boolean[20];
         
-        String[][] poltronas        = new String[3][4];
-        boolean[][] poltronasStatus = new boolean[3][4];
+        //preenchendo vetor de Lugares para config inicial
+        for (int i = 0; i < 20; i++) {
+            vetLugares[i] = "M" + (i + 1);
+            vetLugaresOcupados[i] = false;
+        }
         
-       
-        preenchePoltronas(poltronas);
-        preenchePoltronas(poltronasStatus);
-        listaPoltronas(poltronas);
-        System.out.println(calculaPctg(poltronasStatus));
-    }
-    static void preenchePoltronas(String[][] poltronas){
-        for (int i = 0; i < poltronas.length; i++) {
-            for (int j = 0; j < poltronas[i].length; j++) {
-                poltronas[i][j] = "L" + i + j;
+        while (true){
+            System.out.println("____________Avião____________");
+            System.out.println("""
+                               ____________________________________
+                               1 - Reservar Lugar;
+                               2 - liberar Lugar;
+                               3 - listar Lugares livres.
+                               ____________________________________""");
+            System.out.print("O que deseja fazer (digite o numero da opção)? ");
+            int menu = sc.nextInt();
+            
+            switch(menu) {
+                case 1 -> reservarLugar(vetLugaresOcupados, vetLugares);
+                case 2 -> liberarLugar(vetLugaresOcupados, vetLugares);
+                case 3 -> listarLugaresLivres(vetLugaresOcupados, vetLugares);
+                default -> System.out.println("Opção invalida");
+            }
+            
+            System.out.print("Deseja sair do programa [S/N]? ");
+            char sairProg = sc.next().toUpperCase().charAt(0);
+            
+            if (sairProg == 'S') {
+                break;
             }
         }
     }
-    static void preenchePoltronas(boolean[][] poltronasStatus){
-        for (int i = 0; i < poltronasStatus.length; i++) {
-            for (int j = 0; j < poltronasStatus[i].length; j++) {
-                poltronasStatus[i][j] = false;
+    // função para listar Lugares
+    static void listarLugaresLivres (boolean[] vetLugaresOcupados, String[] vetLugares) {
+        System.out.println("Lugares LIVRES:");
+        for (int i = 0; i < vetLugaresOcupados.length; i++) {
+            if(!vetLugaresOcupados[i]){
+                System.out.print(vetLugares[i] + " | ");
             }
         }
+        System.out.println();
     }
-    static void listaPoltronas(String[][] poltronas){
-        for (int i = 0; i < poltronas.length; i++) {
-            for (int j = 0; j < poltronas[i].length; j++) {
-                System.out.print(poltronas[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-    static double calculaPctg(boolean[][] poltronasStatus){
-        int countOcupados = 0;
+    // função para liberar Lugares
+    static void liberarLugar (boolean[] vetLugaresOcupados, String[] vetLugares) {
+        Scanner sc = new Scanner(System.in); // traz a classe para a função
         
-        for (int i = 0; i < poltronasStatus.length; i++) {
-            for (int j = 0; j < poltronasStatus[i].length; j++) {
-                if (poltronasStatus[i][j]){
-                    countOcupados++;
+        System.out.print("Digite o código da Lugar: "); 
+        String escolhaLugar = sc.next().toUpperCase();
+        boolean econtrado = false;
+        
+        for (int i = 0; i < vetLugares.length; i++) {
+            if (escolhaLugar.equals(vetLugares[i])){
+                econtrado = true;
+                
+                if (!vetLugaresOcupados[i]) {
+                    System.out.println("A Lugar já está livre.");
+                } else if (vetLugaresOcupados[i]) {
+                    vetLugaresOcupados[i] = false;
+                    System.out.println("Lugar Liberada com sucesso!");
                 }
             }
         }
-        double pctg = countOcupados / (poltronasStatus.length * poltronasStatus[0].length) * 100;
+        if (!econtrado) { // caso a input não tenha sido encontrado no vetor
+            System.out.println("Lugar não encontrada digite o código corretamente!");
+        }
+    }
+    // função para reservar Lugares
+    static void reservarLugar (boolean[] vetLugaresOcupados, String[] vetLugares) {
+        Scanner sc = new Scanner(System.in); // tras o sacanner pra função
         
-        return pctg;
+        System.out.print("Digite o código da Lugar: ");
+        String escolhaLugar = sc.next().toUpperCase();
+        boolean econtrado = false;
+        
+        for (int i = 0; i < vetLugares.length; i++) {
+            if (escolhaLugar.equals(vetLugares[i])){ //verifica se a escolha está no vetor
+                econtrado = true;
+                
+                if (!vetLugaresOcupados[i]) { // se Lugar estiver livre
+                    vetLugaresOcupados[i] = true; // Lugar fica ocupada
+                    System.out.println("Reserva do Lugar " + vetLugares[i] + " realizado com sucesso!"); 
+                } else if (vetLugaresOcupados[i]) { // caso esteja ocupada
+                    System.out.println("Lugar Ocupado!");
+                }
+            }
+        }
+        if (!econtrado) { // caso a input não tenha sido encontrado no vetor
+            System.out.println("Lugar não encontrada digite o código corretamente!");
+        }
     }
 }
